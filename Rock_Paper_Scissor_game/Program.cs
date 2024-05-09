de7e851017f92336a1d952fd45c0670517aceba3
Abbasi3333 <ubaidabbasi291@gmail.com>
2024-05-09 09:05:12 -0700

    Add project files.

---
+++ Rock_Paper_Scissor_game/Program.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissor_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String Player1 , Player2 ;
+            
+            int Player1score = 0 , Player2score = 0 ;
+            Console.Write("Enter Your first attempt to win-Player-1:");
+            Player1 = Console.ReadLine();
+            Console.Write("Enter Your first attempt to win-Player-2:");
+            Player2 = Console.ReadLine();
+            if (Player1 == "Rock" && Player2 == "Paper")
+            {
+                Console.WriteLine("Player 2 Win!!!");
+                Player2score++;
+                Console.WriteLine("Player2 score:" +Player2score);
+                Console.WriteLine("Player1Score:" +Player1score);
+            }
+            else if (Player1 == "Scissor" && Player2 == "Rock")
+            {
+                Console.WriteLine("Player 2 Win!!!");
+                Player2score++;
+                Console.WriteLine("Player2 score:" + Player2score);
+                Console.WriteLine("Player1Score:" + Player1score);
+            }
+            else if (Player1 == "Paper" && Player2 == "Scissor")
+            {
+                Console.WriteLine("Player 2 Win!!!");
+                Player2score++;
+                Console.WriteLine("Player2 score:" + Player2score);
+                Console.WriteLine("Player1Score:" + Player1score);
+            }
+            else if (Player1 == "Rock" && Player2 == "Scissor")
+            {
+                Console.WriteLine("Player 1 Win!!!");
+                Player1score++;
+                Console.WriteLine("Player1 score:" + Player1score);
+                Console.WriteLine("Player2Score:" + Player2score);
+            }
+            else if (Player1 == "Paper" && Player2 == "Rock")
+            {
+                Console.WriteLine("Player 1 Win!!!");
+                Player1score++;
+                Console.WriteLine("Player1 score:" + Player1score);
+                Console.WriteLine("Player2Score:" + Player2score);
+            }
+            else if (Player1 == "Scissor" && Player2 == "Paper")
+            {
+                Console.WriteLine("Player 1 Win!!!");
+                Player1score++;
+                Console.WriteLine("Player1 score:" + Player1score);
+                Console.WriteLine("Player2Score:" + Player2score);
+            }
+            else
+            {
+                Console.WriteLine("Invalid move");
+            }
+            //another move
+            Console.Write("Enter Your first attempt to win-Player-1:");
+            Player1 = Console.ReadLine();
+            Console.Write("Enter Your first attempt to win-Player-2:");
+            Player2 = Console.ReadLine();
+            if (Player1 == "Rock" && Player2 == "Paper")
+            {
+                Console.WriteLine("Player 2 Win!!!");
+                Player2score++;
+                Console.WriteLine("Player2 score:" + Player2score);
+                Console.WriteLine("Player1Score:" + Player1score);
+            }
+            else if (Player1 == "Scissor" && Player2 == "Rock")
+            {
+                Console.WriteLine("Player 2 Win!!!");
+                Player2score++;
+                Console.WriteLine("Player2 score:" + Player2score);
+                Console.WriteLine("Player1Score:" + Player1score);
+            }
+            else if (Player1 == "Paper" && Player2 == "Scissor")
+            {
+                Console.WriteLine("Player 2 Win!!!");
+                Player2score++;
+                Console.WriteLine("Player2 score:" + Player2score);
+                Console.WriteLine("Player1Score:" + Player1score);
+            }
+            else if (Player1 == "Rock" && Player2 == "Scissor")
+            {
+                Console.WriteLine("Player 1 Win!!!");
+                Player1score++;
+                Console.WriteLine("Player1 score:" + Player1score);
+                Console.WriteLine("Player2Score:" + Player2score);
+            }
+            else if (Player1 == "Paper" && Player2 == "Rock")
+            {
+                Console.WriteLine("Player 1 Win!!!");
+                Player1score++;
+                Console.WriteLine("Player1 score:" + Player1score);
+                Console.WriteLine("Player2Score:" + Player2score);
+            }
+            else if (Player1 == "Scissor" && Player2 == "Paper")
+            {
+                Console.WriteLine("Player 1 Win!!!");
+                Player1score++;
+                Console.WriteLine("Player1 score:" + Player1score);
+                Console.WriteLine("Player2Score:" + Player2score);
+            }
+            else
+            {
+                Console.WriteLine("Invalid move");
+            }
+            //another move
+            Console.Write("Enter Your first attempt to win-Player-1:");
+            Player1 = Console.ReadLine();
+            Console.Write("Enter Your first attempt to win-Player-2:");
+            Player2 = Console.ReadLine();
+            if (Player1 == "Rock" && Player2 == "Paper")
+            {
+                Console.WriteLine("Player 2 Win!!!");
+                Player2score++;
+                Console.WriteLine("Player2 score:" + Player2score);
+                Console.WriteLine("Player1Score:" + Player1score);
+            }
+            else if (Player1 == "Scissor" && Player2 == "Rock")
+            {
+                Console.WriteLine("Player 2 Win!!!");
+                Player2score++;
+                Console.WriteLine("Player2 score:" + Player2score);
+                Console.WriteLine("Player1Score:" + Player1score);
+            }
+            else if (Player1 == "Paper" && Player2 == "Scissor")
+            {
+                Console.WriteLine("Player 2 Win!!!");
+                Player2score++;
+                Console.WriteLine("Player2 score:" + Player2score);
+                Console.WriteLine("Player1Score:" + Player1score);
+            }
+            else if (Player1 == "Rock" && Player2 == "Scissor")
+            {
+                Console.WriteLine("Player 1 Win!!!");
+                Player1score++;
+                Console.WriteLine("Player1 score:" + Player1score);
+                Console.WriteLine("Player2Score:" + Player2score);
+            }
+            else if (Player1 == "Paper" && Player2 == "Rock")
+            {
+                Console.WriteLine("Player 1 Win!!!");
+                Player1score++;
+                Console.WriteLine("Player1 score:" + Player1score);
+                Console.WriteLine("Player2Score:" + Player2score);
+            }
+            else if (Player1 == "Scissor" && Player2 == "Paper")
+            {
+                Console.WriteLine("Player 1 Win!!!");
+                Player1score++;
+                Console.WriteLine("Player1 score:" + Player1score);
+                Console.WriteLine("Player2Score:" + Player2score);
+            }
+            else
+            {
+                Console.WriteLine("Invalid move");
+            }
+            //another move
+            Console.Write("Enter Your first attempt to win-Player-1:");
+            Player1 = Console.ReadLine();
+            Console.Write("Enter Your first attempt to win-Player-2:");
+            Player2 = Console.ReadLine();
+            if (Player1 == "Rock" && Player2 == "Paper")
+            {
+                Console.WriteLine("Player 2 Win!!!");
+                Player2score++;
+                Console.WriteLine("Player2 score:" + Player2score);
+                Console.WriteLine("Player1Score:" + Player1score);
+            }
+            else if (Player1 == "Scissor" && Player2 == "Rock")
+            {
+                Console.WriteLine("Player 2 Win!!!");
+                Player2score++;
+                Console.WriteLine("Player2 score:" + Player2score);
+                Console.WriteLine("Player1Score:" + Player1score);
+            }
+            else if (Player1 == "Paper" && Player2 == "Scissor")
+            {
+                Console.WriteLine("Player 2 Win!!!");
+                Player2score++;
+                Console.WriteLine("Player2 score:" + Player2score);
+                Console.WriteLine("Player1Score:" + Player1score);
+            }
+            else if (Player1 == "Rock" && Player2 == "Scissor")
+            {
+                Console.WriteLine("Player 1 Win!!!");
+                Player1score++;
+                Console.WriteLine("Player1 score:" + Player1score);
+                Console.WriteLine("Player2Score:" + Player2score);
+            }
+            else if (Player1 == "Paper" && Player2 == "Rock")
+            {
+                Console.WriteLine("Player 1 Win!!!");
+                Player1score++;
+                Console.WriteLine("Player1 score:" + Player1score);
+                Console.WriteLine("Player2Score:" + Player2score);
+            }
+            else if (Player1 == "Scissor" && Player2 == "Paper")
+            {
+                Console.WriteLine("Player 1 Win!!!");
+                Player1score++;
+                Console.WriteLine("Player1 score:" + Player1score);
+                Console.WriteLine("Player2Score:" + Player2score);
+            }
+            else
+            {
+                Console.WriteLine("Invalid move");
+            }
+            Console.ReadLine();
+        }
+    }
+}
